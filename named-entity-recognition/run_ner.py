ad7b8ba198549ea72cbb9b691ff3393aa6ef1691
Ghadeer Mobasher <53903145+mobashgr@users.noreply.github.com>
2022-11-17 16:47:45 +0100

    Original Fine-tuning approach

--- named-entity-recognition/run_ner.py
+++ named-entity-recognition/run_ner.py
@@ -13,7 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" Fine-tuning the library models for named entity recognition on CoNLL-2003. """
+"Original Fine-tuning approach from BioBERT-Pytorch"
+""" Fine-tuning the library models for named entity recognition on CoNLL-2003"""
 
 
 import logging
