f653ab6e4f232b48f1e0a499ffb2b091def73073
Ghadeer Mobasher <53903145+mobashgr@users.noreply.github.com>
2022-11-17 16:38:27 +0100

    Adding License

--- named-entity-recognition/download.sh
+++ named-entity-recognition/download.sh
@@ -2,8 +2,9 @@
 
 #!/bin/bash
 #
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+# These NER datasets are directly 
+#reterived from BioBERT (https://github.com/dmis-lab/biobert) via this link (https://drive.google.com/file/d/1cGqvAm9IZ_86C4Mj7Zf-w9CFilYVDl8j/view) and
+#BioRED dataset is publically avaliable in https://ftp.ncbi.nlm.nih.gov/pub/lu/BioRED/
 
 gdown https://drive.google.com/uc?id=1nHH3UYpQImQhBTei5HiTcAAFBvsfaBw0
 unzip datasets.zip
