2775940dfa5f500f08b8e13d97e9047c31d1685c
mobashgr <ghadeer.mobasher@h-its.org>
2022-11-17 16:26:46 +0100

    WELT

---
+++ named-entity-recognition/download.sh
@@ -0,0 +1,18 @@
+
+
+#!/bin/bash
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+gdown https://drive.google.com/uc?id=1nHH3UYpQImQhBTei5HiTcAAFBvsfaBw0
+unzip datasets.zip
+rm -r datasets.zip
+
+
+echo "Bio dataset download done!"
+
+
+
+
+
