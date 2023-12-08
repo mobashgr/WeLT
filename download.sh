e55926436269b15f0b95fba0b9a3ba11c74209d8
Ghadeer Mobasher <53903145+mobashgr@users.noreply.github.com>
2023-12-08 14:50:47 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -4,8 +4,18 @@
 #reterived from BioBERT (https://github.com/dmis-lab/biobert) via this link (http://nlp.dmis.korea.edu/projects/biobert-2020-checkpoints/datasets.tar.gz) and
 #BioRED dataset is publically available at https://ftp.ncbi.nlm.nih.gov/pub/lu/BioRED/
 
-gdown https://drive.google.com/uc?id=1nHH3UYpQImQhBTei5HiTcAAFBvsfaBw0
-unzip datasets.zip
-rm -r datasets.zip
+set -e
 
+# Configure download location
+DOWNLOAD_PATH="$BIOBERT_DATA"
+if [ "$BIOBERT_DATA" == "" ]; then
+    echo "BIOBERT_DATA not set; downloading to default path ('data')."
+    DOWNLOAD_PATH="./data"
+fi
+DOWNLOAD_PATH_TAR="$DOWNLOAD_PATH.zip"
+
+# Download datasets
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1nHH3UYpQImQhBTei5HiTcAAFBvsfaBw0' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1nHH3UYpQImQhBTei5HiTcAAFBvsfaBw0" -O "$DOWNLOAD_PATH_TAR" && rm -rf /tmp/cookies.txt
+jar xvf "$DOWNLOAD_PATH_TAR"
+rm "$DOWNLOAD_PATH_TAR"
 echo "Bio dataset download done!"
