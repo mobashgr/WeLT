27d1af232c0133ded7eacb7f22fe1333ec13f5c4
Ghadeer Mobasher <53903145+mobashgr@users.noreply.github.com>
2023-12-08 14:03:27 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 # These NER datasets are directly 
-#reterived from BioBERT (https://github.com/dmis-lab/biobert) via this link (https://drive.google.com/file/d/1cGqvAm9IZ_86C4Mj7Zf-w9CFilYVDl8j/view) and
-#BioRED dataset is publically avaliable in https://ftp.ncbi.nlm.nih.gov/pub/lu/BioRED/
+#reterived from BioBERT (https://github.com/dmis-lab/biobert) via this link (http://nlp.dmis.korea.edu/projects/biobert-2020-checkpoints/datasets.tar.gz) and
+#BioRED dataset is publically available at https://ftp.ncbi.nlm.nih.gov/pub/lu/BioRED/
 
 gdown https://drive.google.com/uc?id=1nHH3UYpQImQhBTei5HiTcAAFBvsfaBw0
 unzip datasets.zip
